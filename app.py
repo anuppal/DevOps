6d287205ad78391cc0c8237406155ff943e92ca5
Anirudh <anirudh.uppal@gmail.com>
2024-03-03 15:25:01 +0530

    Commit Code-5

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
 #A simple Flask application
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    #Return a simple 'Hello, World!' message.
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
