3800d880536f471a0eb63798ef245c9c1975fe52
Anirudh Uppal <10473964+anuppal@users.noreply.github.com>
2024-03-03 16:04:41 +0530

    Update-2 app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+"""
+This module defines a simple Flask application that prints 'Hello, World!' when accessed.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """Return a simple 'Hello, World!' message."""
     return 'Hello, World!'
 
 if __name__ == '__main__':
